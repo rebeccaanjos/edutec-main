af28b82526457ae4421be16a0ec58644d271023a
rebecca anjos <rebecca.anjos30@gmail.com>
2024-11-27 22:44:11 +0000

    HTML e CSS finalizado

---
+++ conteudo/conteudo.js
@@ -0,0 +1,67 @@
+let contador = 0;
+let contents = [];
+
+// Carregar o conteúdo do data.json
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    contents = data.contents;
+    loadContent(contador); // Carrega o primeiro conteúdo inicialmente
+  })
+
+function loadContent(index) {
+  if (index >= 0 && index < contents.length) {
+    const content = contents[index];
+
+    // Monta o conteúdo HTML dinamicamente
+    let html = content.mainContent;
+
+    if (content.p) {
+      html += content.p;
+    }
+
+    // Verifica se existem itens na lista e cria a lista dinamicamente abaixo do parágrafo
+    if (content.listItems && content.listItems.length > 0) {
+      html += '<ul>';
+      content.listItems.forEach(item => {
+        html += `<li>${item}</li>`;
+      });
+      html += '</ul>';
+    }
+
+    if (content.imageHTML) {
+      html += content.imageHTML;
+    }
+
+    document.getElementById('main-content').innerHTML = html;
+
+    
+    // Controle de visibilidade das setas
+    document.querySelector('.seta-voltar').style.display = index > 0 ? 'block' : 'none';
+
+    // Esconde a seta de próximo na última página
+    if (index === 10) { // Página 11
+      document.querySelector('.seta').style.display = 'none';
+    } else {
+      document.querySelector('.seta').style.display = 'block';
+    }
+  }
+}
+
+// Carrega a próxima página
+document.getElementById('seta').addEventListener('click', function (event) {
+  event.preventDefault();
+  if (contador < contents.length - 1) { // Limita a navegação até a página 11
+    contador++;
+    loadContent(contador);
+  }
+});
+
+// Carrega a página anterior
+document.getElementById('seta-voltar').addEventListener('click', function (event) {
+  event.preventDefault();
+  if (contador > 0) { // Limita a navegação para não ultrapassar o índice 0
+    contador--;
+    loadContent(contador);
+  }
+});
---
+++ quiz/pages/questoes/questoes.js
@@ -0,0 +1,145 @@
+let quiz = {}
+let pontos = 0
+let pergunta = 1
+let resposta = ""
+let idInputResposta = ""
+let respostaCorretaId = ""
+
+async function buscarPerguntas() {
+    const urlDados = "../../data.json"
+
+    await fetch(urlDados).then(resposta => resposta.json()).then(dados => {
+        dados.quiz.forEach(dado => {
+            quiz = dado
+        })
+    })
+}
+
+function montarPergunta() {
+    const main = document.querySelector("main")
+
+    main.innerHTML = `
+            <section class="pergunta">
+                <p>Pergunta ${pergunta}/5</p>
+                
+                <div class="barra_progresso">
+                    <div style = "width: ${pergunta * 20}%"></div>
+                </div>
+
+                <h2></h2>
+
+                <h2>${quiz.questions[pergunta-1].question}</h2>
+                
+            </section>
+
+            <section class="alternativas">
+                <form action="">
+                    <label for="alternativa_a">
+                        <input type="radio" id="alternativa_a" name="alternativa" value="${quiz.questions[pergunta-1].options[0]}">
+                        <div>
+                            ${quiz.questions[pergunta-1].options[0]}
+                        </div>
+                    </label>
+
+                    <label for="alternativa_b">
+                        <input type="radio" id="alternativa_b" name="alternativa" value="${quiz.questions[pergunta-1].options[1]}">
+
+                        <div>
+                            ${quiz.questions[pergunta-1].options[1]}
+                        </div>
+                    </label>
+
+                    <label for="alternativa_c" >
+                        <input type="radio" id="alternativa_c" name="alternativa" value="${quiz.questions[pergunta-1].options[2]}">
+
+                        <div>
+                            ${quiz.questions[pergunta-1].options[2]}
+                        </div>
+                    </label>
+
+                    <label for="alternativa_d">
+                        <input type="radio" id="alternativa_d" name="alternativa" value="${quiz.questions[pergunta-1].options[3]}">
+
+                        <div>
+                            ${quiz.questions[pergunta-1].options[3]}    
+                        </div>
+                    </label>
+
+                </form>
+                
+                <a href="">
+                    <button>Responder</button>
+                </a>
+
+            </section>
+        `
+}
+
+function guardarResposta(evento) {
+    resposta = evento.target.value
+    idInputResposta = evento.target.id
+
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta() {
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+    
+
+    if (pergunta === 5) {
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
+    if (resposta === quiz.questions[pergunta-1].answer) {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos + 1  
+    } else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id","errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id","correta")
+    }
+
+    
+    event.preventDefault();
+    pergunta = pergunta + 1
+}
+
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
+
+    window.location.href = "../ranking/ranking.html"
+    event.preventDefault();
+}
+
+function proximaPergunta() {
+    event.preventDefault();
+    montarPergunta()
+    adicionarEventoInputs()
+}
+
+function adicionarEventoInputs() {
+    const inputsResposta = document.querySelectorAll(".alternativas input")
+    inputsResposta.forEach(input => {
+        input.addEventListener("click", guardarResposta)
+
+        if (input.value === quiz.questions[pergunta-1].answer) {
+            respostaCorretaId = input.id
+        }
+    })
+}
+ 
+async function iniciar() {
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventoInputs()
+    
+    
+
+}
+
+iniciar()
